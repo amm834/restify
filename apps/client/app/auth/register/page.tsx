e3168c2878480913a2e4bfdc90b10f943622072e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-01-31 16:40:38 +0630

    refactor(ui): change submit button to block

--- apps/client/app/auth/register/page.tsx
+++ apps/client/app/auth/register/page.tsx
@@ -116,7 +116,7 @@ export default function RegisterPage() {
                     </p>
                 </div>
 
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" className="min-w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
 
         </div>
